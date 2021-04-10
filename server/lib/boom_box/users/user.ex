58e9441c180c0f962cbb5453743a400934bd3bab
Xiao Tao <61165887+xiao-tao518@users.noreply.github.com>
2021-04-09 21:25:20 -0400

    Add files via upload

--- server/lib/boom_box/users/user.ex
+++ server/lib/boom_box/users/user.ex
@@ -14,6 +14,15 @@ defmodule BoomBox.Users.User do
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:email, :password_hash])
+    |> add_password_hash(attrs["password"])
     |> validate_required([:email, :password_hash])
+  end
+  
+  def add_password_hash(cset, nil) do
+    cset
+  end
+  
+  def add_password_hash(cset, password) do
+    change(cset, Argon2.add_hash(password))
   end
 end
